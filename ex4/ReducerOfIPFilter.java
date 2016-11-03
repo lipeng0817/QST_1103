5b818496e6bb666958d1f1b85e4cd287400c3aba
lipeng0817 <1394831919@qq.com>
2016-11-03 16:44:14 +0800

    Update ReducerOfIPFilter.java

--- ex4/ReducerOfIPFilter.java
+++ ex4/ReducerOfIPFilter.java
@@ -1,12 +1,25 @@
 package No4;
 
+
+import java.util.HashSet;
 import java.util.Scanner;
+import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class ReducerOfIPFilter {
 	public static void main(String[] args){
+		Set<String> set = new HashSet();
 		Scanner scanner = new Scanner(System.in);
 		while (scanner.hasNext()){
 			String line = scanner.nextLine();
+			String pattern = "(\\d+.\\d+.\\d+.\\d+)\t(.*)";
+			Pattern r = Pattern.compile(pattern);
+			Matcher m = r.matcher(line);
+			if(m.find()){
+			 set.add(m.group(1));
+			} 
 		}
-	}
+		System.out.println(set.size());	
+	}	
 }
